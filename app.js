d554d05ffa31a884b2d6a3a02f320288450d73de
blackpearl15 <yashanand2477@gmail.com>
2022-10-06 23:53:50 +0530

    level 3 authentication done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const ejs = require("ejs");
 
 const app = express();
@@ -22,9 +22,6 @@ const userSchema = new mongoose.Schema({
   password : String
 });
 
-const secret = "Thisisourlittlesecret.";
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ["password"] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -45,7 +42,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
       email : req.body.username,
-      password:req.body.password
+      password:md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -59,7 +56,7 @@ app.post("/register",function(req,res){
 app.post("/login",function(req,res){
 
      const username = req.body.username;
-     const password = req.body.password;
+     const password = md5(req.body.password);
 
      User.findOne({email:username},function(err,foundUser){
         if(err)
