9eaa5c130c8292950c4548ec135f803c7ddfcf7b
blackpearl15 <yashanand2477@gmail.com>
2022-10-08 11:23:36 +0530

    Level 6 Authentication done using Google OAuth 2.0

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const ejs = require("ejs");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -31,10 +33,12 @@ mongoose.connect(process.env.API);
 
 const userSchema = new mongoose.Schema({
   email : String,
-  password : String
+  password : String,
+  googleId : String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 const User = new mongoose.model("User",userSchema);
@@ -49,10 +53,35 @@ passport.deserializeUser(function(user,done){
   done(null,user);
 });
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));
+
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: "/login" }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/secrets');
+    });
 
 app.get("/login",function(req,res){
   res.render("login");
@@ -79,6 +108,36 @@ app.get("/secrets",function(req,res){
    else{
      res.render("/login");
    }
+});
+
+app.get("/submit",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }
+  else{
+    res.render("/login");
+  }
+});
+
+app.post("/submit",function(req,res){
+
+    const submittedSceret = req.body.secret;
+
+    console.log(req.body._id);
+
+    User.findOne(req.user._id,function(err,foundUser){
+      if(err){
+        console.log(err);
+      }
+      else{
+        if(foundUser){
+          foundUser.secret = submittedSceret;
+          foundUser.save(function(){
+            res.redirect("/secrets");
+          });
+        }
+      }
+    });
 });
 
 app.post("/register",function(req,res){
